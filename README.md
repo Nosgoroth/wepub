87e0bcc06201e8dee7d12ec3d2e79cd79f675f75
cnavarroestrella <carlos.navarro.estrella@gmail.com>
2018-02-24 15:07:27 +0100

    Refactor

--- README.md
+++ README.md
@@ -19,16 +19,68 @@ Heavily based on [web2epub](https://github.com/rupeshk/web2epub) by **rupeshk**,
 ## Downloading from the web
 
 1. Create a config `.py` file in the configs folder, named as you like. You can copy the provided `example.py`.
-2. Set at least the `title`, `author`, `outfile` and `urls` variables. Each URL will become a chapter. The path for `outfile` is relative to the root of the project, not the config file.
-3. If you have a cover image, set the `cover` variable to the relative path of the image, or an URL.
-4. Consider adding filters to the body (`filters`) and the title (`titlefilters`). These are tuples where the first and second elements will be passed as parameters to `re.sub`
-5. You can decide if you want to add the page title as a header for the chapter by setting `title_as_header`. It defaults to `True`.
+2. Set at least the `title`, `author` and `outfile` variables. The path for `outfile` is relative to the root of the project, not the config file.
+3.  Set the `urls` array. Each URL will become a chapter. See the section below on how to add static chapters.
+4. If you have a cover image, set the `cover` variable to the relative path of the image, or an URL.
+5. Consider adding filters to the body (`filters`) and the title (`titlefilters`). These are tuples where the first and second elements will be passed as parameters to `re.sub`
+6. You can decide if you want to add the html page title as a header for the chapter by setting `title_as_header`. It defaults to `True`.
+
+It should look like this:
+
+```py
+# -*- coding: utf-8 -*-
+
+title = "My test ebook"
+author = "Various Wikipedia collaborators"
+outfile = "out/My test ebook.epub"
+
+cover = "covers/some_image.jpg" #can also be a url
+
+title_as_header = True
+
+urls = [
+    "https://en.wikipedia.org/wiki/E-book",
+    "https://en.wikipedia.org/wiki/Python_(programming_language)",
+    "https://en.wikipedia.org/wiki/Initial_mass_function",
+    "https://en.wikipedia.org/wiki/Airport_and_airline_management"
+]
+
+filters = [
+    [ r'<a[^\>]+>(.*?)</a>', r'\1' ], #removes links but keeps the text inside
+    [ r'\[[^\]]*\]', r'' ]  #removes stuff within brackets
+]
+
+titlefilters = [
+    [ r'Airport', r'Starbase' ]
+]
+```
 
 You can now run this config, which we'll imagine you named `myconfig.py`, by going down to the root folder of the project and running:
 
 ```bash
 python wepub.py myconfig
 ```
+
+### Static chapters
+
+You can add static chapters by including HTML as a string in the `urls` array. You can define the chapter title by including a `<title>` element somewhere within the string.
+
+```py
+urls = [
+    '''
+        <title>A leading test image</title>
+        <p><img src="https://i.imgur.com/ciWcTlk.jpg" /></p>
+        <p>What did you expect?</p>
+    ''',
+    "https://en.wikipedia.org/wiki/E-book",
+    "https://en.wikipedia.org/wiki/Python_(programming_language)",
+    #...
+]
+```
+
+
+
+### Extra options
 
 There are options that you can consult with `--help`, and they include these:
 
---
+++ wepubutils.py
@@ -0,0 +1,352 @@
+
+# Standard modules
+import os, sys, re, time, mimetypes, urlparse, zipfile, cgi, hashlib
+# Third party modules
+import requests
+from readability.readability import Document
+from BeautifulSoup import BeautifulSoup,Tag
+# App modules
+import wepubtemplates
+
+
+
+class EpubProcessor:
+
+	epub = None
+	cover = None
+	info = None
+
+	def __init__(self, options):
+		self.options = options
+
+	def make(self):
+		self.processCover()
+		self.makeInfo()
+		
+		# The first file must be named "mimetype"
+		self.epub = EpubZipFile(self.options.outfile, 'w', zipfile.ZIP_DEFLATED)
+		self.epub.writestr("mimetype", "application/epub+zip", zipfile.ZIP_STORED)
+		# We need an index file, that lists all other HTML files
+		# This index file itself is referenced in the META_INF/container.xml file
+		self.epub.writestr("META-INF/container.xml", wepubtemplates.container_tpl)
+
+		self.epub.writestr('OEBPS/cover.html', wepubtemplates.cover_tpl % self.info)
+		if self.cover is not None:
+			self.epub.write(
+				os.path.abspath(self.cover),
+				'OEBPS/images/cover'+os.path.splitext(self.cover)[1],
+				zipfile.ZIP_DEFLATED
+				)
+
+
+		self.info['manifest'] = ""
+		self.info['spine'] = ""
+		self.info['toc']= ""
+
+		for i,url in enumerate(self.options.urls):
+			(deltamanifest, deltaspine, deltatoc) = self.processUrl(url, i)
+			self.info['manifest'] += deltamanifest
+			self.info['spine'] += deltaspine
+			self.info['toc'] += deltatoc
+
+		# Finally, write the index and toc
+		self.epub.writestr('OEBPS/stylesheet.css', wepubtemplates.stylesheet_tpl)
+		self.epub.writestr('OEBPS/Content.opf', wepubtemplates.index_tpl % self.info)
+		self.epub.writestr('OEBPS/toc.ncx', wepubtemplates.toc_tpl % self.info)
+
+		self.epub.close()
+
+
+	def makeInfo(self):
+		try: isbn = self.options.isbn
+		except: isbn = None
+
+		try: description = self.options.description
+		except: description = None
+
+		try: title = self.options.title
+		except: title = 'Title'
+
+		try: author = self.options.author
+		except: author = ''
+
+		if self.cover:
+			cpath = 'images/cover' + os.path.splitext(os.path.abspath(self.cover))[1]
+			ctype = mimetypes.guess_type(os.path.basename(os.path.abspath(self.cover)))[0]
+		else:
+			cpath = wepubtemplates.b64emptygif
+			ctype = 'image/gif'
+
+		self.info = dict(title=title,
+			author=author,
+			rights='Copyright respective authors',
+			publisher='',
+			ISBN=isbn,
+			subject='Web content',
+			description=description,
+			date=time.strftime('%Y-%m-%d'),
+			front_cover= cpath,
+			front_cover_type = ctype
+			)
+
+	def processCover(self):
+		try: self.cover = self.options.cover
+		except: return False
+		if not self.cover: return False
+
+		print "Processing cover...",
+		sys.stdout.flush()
+		self.cover = processImage(self.cover, ignoreCache=self.options.nocache)
+		print "done"
+
+	def processUrl(self, url, index):
+
+		if not url.lower().startswith("http"):
+			return self.processStaticPage(url, index)
+
+		print
+		print "Reading url no. %s of %s --> %s " % (index+1, len(self.options.urls), url)
+
+		r = retrieveUrl(
+			url,
+			transforms=self.options.filters,
+			titleTransforms=self.options.titlefilters,
+			ignoreCache=self.options.nocache,
+			ignoreReadability=self.options.nordbcache or self.options.nocache,
+			versionId=self.options.versionid
+		)
+
+		print "   ", "Retrieved from", r["source"]
+
+		readable_article = r["article"]
+		readable_title = r["title"]
+		
+		if not readable_article:
+			print "   ", "No content!!!"
+			return ("","","")
+		else:
+			print "   ", "Content size:", len(readable_article)
+
+		print "   ", "["+readable_title+"]"
+
+		if self.options.preview:
+			print readable_article
+			sys.exit()
+
+		return self.writePage(readable_article, readable_title, index, baseUrl=url)
+
+	def processStaticPage(self, staticPageStr, index):
+
+		print
+		print "Processing static page %s of %s... " % (index+1, len(self.options.urls))
+
+		content = staticPageStr
+		title = ''
+
+		r = re.search(r'<title>(.*)</title>', staticPageStr)
+		if r:
+			title = r.group(1)
+			content = content.replace(r.group(0), '')
+
+		content = '<html><head></head><body>%s</body>' % content
+
+		return self.writePage(content, title, index)
+
+
+	def writePage(self, content, title, index, baseUrl=None):
+
+		manifest = '<item id="article_%s" href="article_%s.html" media-type="application/xhtml+xml"/>\n' % (index+1,index+1)
+		spine = '<itemref idref="article_%s" />\n' % (index+1)
+		toc = '<navPoint id="navpoint-%s" playOrder="%s"> <navLabel> <text>%s</text> </navLabel> <content src="article_%s.html"/> </navPoint>' % (index+2,index+2,cgi.escape(title),index+1)
+
+		soup = BeautifulSoup(content)
+		#Add xml namespace
+
+		soup.html["xmlns"] = "http://www.w3.org/1999/xhtml"
+
+		#Insert header
+		body = soup.html.body
+
+		if self.options.title_as_header:
+			h1 = Tag(soup, "h1", [("class", "title")])
+			h1.insert(0, cgi.escape(title))
+			body.insert(0, h1)
+
+		#Add stylesheet path
+		head = soup.find('head')
+		if head is None:
+			head = Tag(soup,"head")
+			soup.html.insert(0, head)
+		link = Tag(soup, "link", [("type","text/css"),("rel","stylesheet"),("href","stylesheet.css")])
+		head.insert(0, link)
+		article_title = Tag(soup, "title")
+		article_title.insert(0, cgi.escape(title))
+		head.insert(1, article_title)
+
+		#Download images
+		for j,image in enumerate(soup.findAll("img")):
+			#Convert relative urls to absolute urls
+			if image["src"].lower().startswith("http"):
+				imgfullpath = image["src"]
+			elif baseUrl:
+				imgfullpath = urlparse.urljoin(baseUrl, image["src"])
+			#Remove query strings from url
+			imgpath = urlparse.urlunsplit(urlparse.urlsplit(imgfullpath)[:3]+('','',))
+			imgfile = os.path.basename(imgpath)
+			filename = 'article_%s_image_%s%s' % (index+1,j+1,os.path.splitext(imgfile)[1])
+
+			print "    Processing image: %s %s" % (j+1, imgpath),
+			sys.stdout.flush()
+
+			filepath = processImage(imgpath, ignoreCache=self.options.nocache)
+			if filepath:
+				print "done"
+
+				with open(filepath) as f: self.epub.writestr('OEBPS/images/'+filename, f.read())
+				
+				image['src'] = 'images/'+filename
+
+				manifest += '<item id="article_%s_image_%s" href="images/%s" media-type="%s"/>\n' % (index+1, j+1, filename,mimetypes.guess_type(filename)[0])
+			
+			else:
+				print "error"
+
+
+		self.epub.writestr('OEBPS/article_%s.html' % (index+1), str(soup))
+		return (manifest, spine, toc)
+
+
+
+
+
+
+
+
+
+
+
+def processImage(urlOrPath, ignoreCache=False, extension=None): #extension with a dot
+	if not urlOrPath: return None
+	if not urlOrPath.lower().startswith("http"): return urlOrPath
+
+	if not os.path.isdir("cache"): os.mkdir("cache")
+
+	hashid = hashlib.sha1(urlOrPath).hexdigest()
+	if not extension:
+		path = urlparse.urlparse(urlOrPath).path
+		extension = os.path.splitext(path)[1]
+	cachefile = os.path.join("cache", hashid+"_img"+extension)
+
+	if os.path.exists(cachefile):
+		return cachefile
+	else:
+		try:
+			r = requests.get(urlOrPath)
+			with open(cachefile, 'wb') as f:
+				f.write(r.content)
+			return cachefile
+		except KeyboardInterrupt:
+			raise
+		except:
+			print "Download error"
+			return None
+
+
+
+
+
+
+
+class RetrieveUrlException(Exception): pass
+
+def retrieveUrl(url, transforms=[], titleTransforms=[], ignoreCache=False, ignoreReadability=False, versionId=None):
+
+	hashsrc = url + (("&v="+str(versionId)) if versionId else "")
+	hashid = hashlib.sha1(hashsrc).hexdigest()
+	htmlcachefile = os.path.join("cache", hashid+"_raw.html")
+	rdbcachefile = os.path.join("cache", hashid+"_rdb.html")
+	rdbtcachefile = os.path.join("cache", hashid+"_rdbt.txt")
+
+	if not os.path.isdir("cache"): os.mkdir("cache")
+
+	html = None
+	rdbhtml = None
+	rdbtitle = None
+	source = None
+
+	if not os.path.exists(rdbcachefile) or not os.path.exists(rdbtcachefile) or ignoreReadability:
+		if not os.path.exists(htmlcachefile) or ignoreCache:
+			print "   ", "Getting from network...",
+			sys.stdout.flush()
+			try:
+				r = requests.get(url)
+				print r.status_code
+			except:
+				print "ERROR"
+				raise
+			html = r.text
+
+			if not html: raise RetrieveUrlException("Couldn't retrieve URL")
+
+			with open(htmlcachefile, 'w') as f: f.write(html.encode('utf-8'))
+			source = "network"
+		else:
+			with open(htmlcachefile, 'r') as f: html = f.read()
+			source = "raw cache"
+
+		if not html: raise RetrieveUrlException("No data")
+
+		print "   ", "Creating readable version...",
+		sys.stdout.flush()
+		
+		readabilitydoc = Document(html)
+		
+		rdbhtml = readabilitydoc.summary().encode('utf-8')
+		rdbtitle = readabilitydoc.short_title().encode('utf-8')
+
+		print "OK"
+
+		for (rx, to) in transforms:
+			try:
+				rdbhtml = re.sub(rx, to, rdbhtml)
+			except:
+				print "   ", "Error with transform", rx, to
+				pass
+
+		for (rx, to) in titleTransforms:
+			try:
+				rdbtitle = re.sub(rx, to, rdbtitle)
+			except:
+				print "   ", "Error with transform", rx, to
+				pass
+
+		with open(rdbcachefile, 'w') as f: f.write(rdbhtml)
+		with open(rdbtcachefile, 'w') as f: f.write(rdbtitle)
+	else:
+		with open(rdbcachefile, 'r') as f: rdbhtml = f.read()
+		with open(rdbtcachefile, 'r') as f: rdbtitle = f.read()
+		source = "cache"
+
+	if not rdbhtml: raise RetrieveUrlException("No content")
+
+	return {
+		"url": url,
+		"versionId": versionId,
+
+		"raw": html,
+		"article": rdbhtml,
+		"title": rdbtitle,
+
+		"rawcachefile": htmlcachefile,
+		"articlecachefile": rdbcachefile,
+		"titlecachefile": rdbtcachefile,
+
+		"source": source,
+	}
+
+
+class EpubZipFile(zipfile.ZipFile):
+	def writestr(self, name, s, compress=zipfile.ZIP_DEFLATED):
+		zipinfo = zipfile.ZipInfo(name, time.localtime(time.time())[:6])
+		zipinfo.compress_type = compress
+		zipfile.ZipFile.writestr(self, zipinfo, s)
