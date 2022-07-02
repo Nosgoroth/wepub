d8538a54c601efc8d59e2162beeb2f686e7e026e
Nosgoroth <nosgoroth@gmail.com>
2022-07-02 12:26:12 +0200

    Add --no-convert flag

--- sendtokindle.py
+++ sendtokindle.py
@@ -13,7 +13,7 @@ from optparse import OptionParser
 import ebookconvert
 
 
-def sendFromConfig(filepath, extraConvertParams=None):
+def sendFromConfig(filepath, extraConvertParams=None, autoConvertMobi=True):
     import config as kindleconfig
     return send(
         filepath,
@@ -21,11 +21,12 @@ def sendFromConfig(filepath, extraConvertParams=None):
         toAddr=kindleconfig.kindle_to,
         gmailUser=kindleconfig.gmail_user,
         gmailPass=kindleconfig.gmail_pass,
-        extraConvertParams=extraConvertParams
+        extraConvertParams=extraConvertParams,
+        autoConvertMobi=autoConvertMobi
     )
 
 def send(filepath, fromAddr=None, toAddr=None, gmailUser=None, gmailPass=None,
-    autoConvertMobi=True, extraConvertParams=None):
+    extraConvertParams=None, autoConvertMobi=True):
     fn, ext = os.path.splitext(filepath)
 
     if not ext == ".mobi" and autoConvertMobi:
@@ -72,6 +73,7 @@ def build_command_line():
 def main():
     parser = build_command_line()
     parser.add_option("--fix-paragraphs", action="store_true", dest="fixparagraphs", help="Fix paragraph styling when converting to MOBI")
+    parser.add_option("--no-convert", action="store_true", dest="noconvert", help="Don't convert to mobi automatically")
     (options, args) = parser.parse_args()
 
     if len(args) < 1:
@@ -82,7 +84,7 @@ def main():
     if options.fixparagraphs:
         extraConvertParams += ebookconvert.ConstantFixParagraphOptions
 
-    sendFromConfig(args[0], extraConvertParams=extraConvertParams)
+    sendFromConfig(args[0], extraConvertParams=extraConvertParams, autoConvertMobi=(not options.noconvert))
 
 if __name__ == '__main__':
 	main()
